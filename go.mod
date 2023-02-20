c52e4293e69b0d07bd47dd404f3bf2d6700e15de
ChrisJon02 <chrisjvunnam@gmail.com>
2023-02-21 01:27:40 +0530

    Go-Project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ChrisJon02/go-microservices
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ChrisJon02/go-microservices/details"
+	"github.com/gorilla/mux"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Checking Application health")
+	response := map[string]string{
+		"status":    "UP",
+		"timestamp": time.Now().String(),
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving the Homepage")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Application is running")
+}
+
+func detailsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Fetching the details")
+	hostname, err := details.GetHostname()
+	if err != nil {
+		panic(err)
+	}
+	IP, _ := details.GetIP()
+	fmt.Println(hostname, IP)
+	response := map[string]string{
+		"hostname": hostname,
+		"Ip":       IP.String(),
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/health", healthHandler)
+	r.HandleFunc("/", rootHandler)
+	r.HandleFunc("/details", detailsHandler)
+	log.Println("Server has started")
+	log.Fatal(http.ListenAndServe(":80", r))
+
+}
